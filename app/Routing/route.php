95df2a90a74b13e6ee77b2ac824bebd49cf29f49
SirG97 <sirgittarus@gmail.com>
2020-04-29 13:46:17 +0100

    Adding our first basic controller

--- app/Routing/route.php
+++ app/Routing/route.php
@@ -3,8 +3,12 @@ require_once __DIR__.'/../../vendor/altorouter/altorouter/AltoRouter.php';
 
 $router = new Altorouter();
 
-$router->map('GET', '/about', '', 'about us');
+$router->map('GET', '/', '', 'home');
 
 $match = $router->match();
-
-var_dump($match);
+ if($match){
+    require_once __DIR__. '/../controllers/BaseController.php';
+    require_once __DIR__. '/../controllers/IndexController.php';
+    $index = new \App\Controllers\IndexController();
+    $index->show();
+ }
