5e10e4e36ee34d0e3249d21953a2ca38f3d1d07f
SirG97 <sirgittarus@gmail.com>
2020-05-01 23:34:33 +0100

    added session manager functionality

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -3,9 +3,17 @@
 
 namespace App\controllers;
 
+use App\Classes\Session;
 
 class DashboardController extends BaseController{
     public function show(){
-        return view('user/dashboard');
+        Session::add('admin', 'you are welcome');
+
+        if(Session::has('admin')){
+            $msg = Session::get('admin');
+        }else{
+            $msg = 'Not defined';
+        }
+        return view('user/dashboard', ['admin' => $msg]);
     }
 }
