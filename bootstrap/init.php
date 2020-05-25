1ed7ff984ad52db6b6420334aa5265530893d587
SirG97 <sirgittarus@gmail.com>
2020-05-25 22:40:28 +0100

    fixing production error

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -10,7 +10,7 @@ if(!isset($_SESSION)) session_start();
 require_once __DIR__.'/../app/config/_env.php';
 
 // Instantiate database class for the application
-new App\Classes\Database();
+//new App\Classes\Database();
 
 //Set custom error handler
 set_error_handler([new App\Classes\ErrorHandler(), 'handleErrors']);
