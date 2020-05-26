24fd8dd2c6da7f803512f020066466b0d542ac6c
SirG97 <sirgittarus@gmail.com>
2020-05-26 12:16:11 +0100

    Uncommented db conn

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -10,10 +10,10 @@ if(!isset($_SESSION)) session_start();
 require_once __DIR__ . '/../App/Config/_env.php';
 
 // Instantiate database class for the application
-//new App\Classes\Database();
+new App\Classes\Database();
 
 //Set custom error handler
-//set_error_handler([new App\Classes\ErrorHandler(), 'handleErrors']);
+set_error_handler([new App\Classes\ErrorHandler(), 'handleErrors']);
 
 //require the routes file
 require_once __DIR__ . '/../App/Routing/routes.php';
