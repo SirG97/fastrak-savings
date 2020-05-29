d9729e5d1efed19c36c0865c8c4a00150b2689e1
SirG97 <sirgittarus@gmail.com>
2020-05-29 11:52:25 +0100

    Fixing ussd pin error

--- App/Controllers/CustomerController.php
+++ App/Controllers/CustomerController.php
@@ -569,35 +569,33 @@ class CustomerController extends BaseController{
         }
 
         if($text == ''){
-            $response = 'CON Please enter your pin';
+            $response = 'CON Please enter your Fastrak pin';
             echo $response;
             exit;
         }else{
             //The first item in the array should be the pin
             $pin = $level[0];
-            $response = 'END Please enter your pin ' . $pin;
-            echo $response;
-            exit;
-           // $is_pin_valid = Pin::find($pin);
-//            if($is_pin_valid == null){
-//                $fraud_count = CustomerController::update_fraud_count($phoneNumber);
-//                // If fraud count returns true, it means this douche bag has tried an invalid pin up to 3 times
-//                if($fraud_count === true){
-//                    $response = 'END You have been barred from using this service';
-//                    echo $response;
-//                    exit;
-//                }else{
-//                    $response = 'You have only '. $fraud_count . ' trial(s) remaining';
-//                    echo $response;
-//                    exit;
-//                }
-//            }else{
-//                $response = 'CON You\'re about to deposit '. $is_pin_valid->amount . 'in your savings.';
-//                $response .= "1. Proceed\n";
-//                $response .= "2. Cancel\n";
-//                echo $response;
-//                exit;
-//            }
+
+            $is_pin_valid = Pin::find($pin);
+            if($is_pin_valid == null){
+                $fraud_count = CustomerController::update_fraud_count($phoneNumber);
+                // If fraud count returns true, it means this douche bag has tried an invalid pin up to 3 times
+                if($fraud_count === true){
+                    $response = 'END You have been barred from using this service';
+                    echo $response;
+                    exit;
+                }else{
+                    $response = 'You have only '. $fraud_count . ' trial(s) remaining';
+                    echo $response;
+                    exit;
+                }
+            }else{
+                $response = 'CON You\'re about to deposit '. $is_pin_valid->amount . 'in your savings.';
+                $response .= "1. Proceed\n";
+                $response .= "2. Cancel\n";
+                echo $response;
+                exit;
+            }
         }
 
     }
