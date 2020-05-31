39b805e187085fa4e3f0bdf38630d78772399a06
SirG97 <sirgittarus@gmail.com>
2020-05-31 16:45:33 +0100

    modification to print input request

--- App/Controllers/CustomerController.php
+++ App/Controllers/CustomerController.php
@@ -349,7 +349,7 @@ class CustomerController extends BaseController{
 
             $level = explode("*", $text);
             header('Content-type: text/plain');
-            echo $_POST["serviceCode"];
+            echo $_POST["phoneNumber"];
             exit;
             //Check if number is registered
             $is_registered_customer = Customer::where('phone', '=', $phoneNumber)->first();
