8e1651e3daa4f00fb51901e462dac4e6658b269d
SirG97 <sirgittarus@gmail.com>
2020-06-05 08:55:47 +0100

    Changed the database connection variable for production

--- App/Classes/Database.php
+++ App/Classes/Database.php
@@ -10,11 +10,11 @@ class Database{
     public function __construct(){
         $db = new Capsule();
         $db->addConnection([
-            'driver' => getenv('DB_DRIVER' , 'mysql'),
-            'host' => getenv('DB_HOST' ,'db4free.net'),
-            'database' => getenv('DB_NAME' , 'cls_charity'),
-            'username' => getenv('DB_USERNAME' , 'sarge_charity'),
-            'password' => getenv('DB_PASSWORD', 'rrwcscrz1_charity'),
+            'driver' => 'mysql',
+            'host' => 'db4free.net',
+            'database' => 'cls_charity',
+            'username' =>  'sarge_charity',
+            'password' => 'DB_PASSWORD', 'rrwcscrz1_charity',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => ''
